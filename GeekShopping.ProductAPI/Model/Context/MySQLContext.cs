75ffe15cad4a1a05297f0ce9fabe45fba84f84ed
juanroas <dijuanroas@gmail.com>
2025-01-27 16:24:31 -0300

    update project, with the first version v1.0.0

---
+++ GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MySqlConnector;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext() { }
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string DbPath = "Server=localhost;DataBase=geek_shopping_product_api;Uid=root;Pwd=admin123;Pipelining=False;";
+                optionsBuilder.UseMySql(
+                       DbPath,
+                        new MySqlServerVersion(new Version(8, 4, 3)),
+                       options => options.EnableRetryOnFailure(2)
+                   );
+            }
+        }
+    }
+}
---
+++ GeekShopping.ProductAPI/Program.cs
@@ -0,0 +1,68 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+//#################################################################################
+var connection = app.Configuration["MySQlConnection:MySQLConnectionString"];
+
+
+builder.Services.AddDbContext<MySQLContext>(options =>
+    options.UseMySql(connection, new MySqlServerVersion(
+                                    new Version(8, 4, 3))));
+
+
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+//#################################################################################
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()){
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
